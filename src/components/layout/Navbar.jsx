1a60e2337e4adcd36b107a94289a6fbeb6da05b0
Jabonga <davidjaythanruiz12@gmail.com>
2025-05-22 17:59:08 -0500

    rspm

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <nav className="bg-music-dark shadow-md sticky top-0 z-50">
       <div className="container mx-auto px-6 py-3 flex justify-between items-center">
         <NavLink to="/" className="text-xl font-bold text-white hover:text-music-primary">
-          Your Band/Artist Name
+          RSPM
         </NavLink>
         <div className="space-x-4">
           <NavLink
